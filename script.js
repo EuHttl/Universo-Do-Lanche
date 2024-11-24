8504b6ccacd500be92c43d7d4beef134844d7cb6
Hyttalo Costa <147499168+EuHttl@users.noreply.github.com>
2024-11-24 03:03:59 +0000

    fix hours and function to calculate the time

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkFuncionamento(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 23;
+    return hora >= 18 && hora <= 23;
 }
 
 const spanItem = document.getElementById("date-span")
